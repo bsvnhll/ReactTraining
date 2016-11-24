cc8c98d5cc2bb5538e1d2209a20c97d864201f59
Pradeep <you@example.com>
2016-11-24 14:24:31 +0530

    day 2

---
+++ TestingDemo/app/tests/components/Clock.test.jsx
@@ -0,0 +1,15 @@
+var React = require('React');
+var ReactDom = require('react-dom');
+var expect = require('expect');
+var Clock = require('Clock');
+var $=require('jQuery');
+//var TestUtils = require('react-addons-test-utils');
+
+describe('Clock', () => {
+  it('should exists', () => {
+    expect(Clock).toExist();
+  });
+});
+
+
+
